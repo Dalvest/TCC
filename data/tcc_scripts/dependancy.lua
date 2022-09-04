54d3098b3b83d4e1d4c764e7b779a74a554eb151
Vertaalfout <104238697+Vertaalfout@users.noreply.github.com>
2022-09-04 13:14:53 -0400

    fixes

---
+++ data/tcc_scripts/dependancy.lua
@@ -0,0 +1,17 @@
+script.on_load(
+function()
+  --Creating a global variable with our addon's information,
+  --so that dependancies can be checked when the game loads
+  _G["TCC_INFO"]={
+      version="1.15", --Keep this updated, For dependancy checks with other mods
+      }
+    --[[ EXAMPLE OF FAILED CHECK
+  if not _G["INFERNO_CORE_INFO"] then
+    Hyperspace.ErrorMessage("Info: It appears that Inferno-Core was not patched before TCC. You may continue, but it is recommended to patch Inferno-Core before the Trash Compactor Collection.")
+  end
+  --]]
+
+
+
+end
+)
